c71d73a18741ff3655f66e20cf7750023ddc36e0
marimunari <54755896+marimunari@users.noreply.github.com>
2020-01-10 15:43:15 -0300

    Correções

--- routes/web.php
+++ routes/web.php
@@ -13,15 +13,9 @@
 
 Route::get('/', 'HeroController@index');
 
-//Route::put('/editar/{id}', function () {
-//    return view('update');
-//});
-
 Route::get('/editar/{id}', 'HeroController@editar')->name('editar');
 
 Route::post('/update/{id}', 'HeroController@update')->name('update');
-
-//Route::post('/update', 'HeroController@editar');
 
 Route::get('/cadastrar', function () {
     return view('register');
